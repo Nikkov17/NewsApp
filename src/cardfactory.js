ac8eb662c3a519ed2903295d192741e57087bd8c
Nikkov17 <kovalyov.k51@gmail.com>
2018-11-21 23:57:48 +0300

    change styles

--- src/cardfactory.js
+++ src/cardfactory.js
@@ -4,15 +4,15 @@ export default function cardFactoryFunction(item) {
     let { author, title, content, description, publishedAt, urlToImage, url } = item;
     let insertString = 
     `<div class="news-item-header">
+        <div class="news-item-header-image-container">
+            ${urlToImage ? `<img class="news-item-header-image" src=${urlToImage}>` : ``}
+        </div>
         <div class="news-item-header-info">
             ${title ? `<div class="news-item-title">${title}</div>` : ``}
             ${author ? `<div class="news-item-author">${author}</div>` : ``}
             ${publishedAt ? `<div class="news-item-publishedat">${formatDateTime(publishedAt)}</div>` : ``}
             ${description ? `<article class="news-item-article">${description}</article>` : ``}
             ${content ? `<article class="news-item-article">${content}</article>` : ``}
-        </div>
-        <div class="news-item-header-image-container">
-            ${urlToImage ? `<img class="news-item-header-image" src=${urlToImage}>` : ``}
             ${url ? `<a class="news-item-link-to-original" href=${url}>Link to original article</a>` : ``}
         </div>
     </div>`;
