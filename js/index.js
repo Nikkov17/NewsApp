f1e1b2e6675979793d1f13e3d6067cb48f9e879d
Nikkov17 <kovalyov.k51@gmail.com>
2018-11-25 18:35:06 +0300

    bug fix

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 import '../css/style.css';
 import '../css/reset.css';
-// import 'https://use.fontawesome.com/releases/v5.5.0/css/all.css';
-import getElements from './getElements';
+import getElements from './getelements';
 
 getElements();
