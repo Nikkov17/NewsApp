e2d3a21df0c14696f9cb747996f2190f2db0711c
Kovalyov Nikolai <kovalyov.k51@gmail.com>
2018-11-28 00:28:38 +0300

    check error(no error on local)

--- js/index.js
+++ js/index.js
@@ -5,6 +5,7 @@ import 'whatwg-fetch';
 import getElements from './getelements';
 import jsonFile from '../loaders/jsonFile.json';
 
+debugger;
 document.querySelector('.lazy-load-js-css').classList.add('hidden');
 
 //check my json loader
