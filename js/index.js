4b8dd88b008a5638ec2c4717e99a6615140cb37b
Nikkov17 <kovalyov.k51@gmail.com>
2018-11-28 00:35:46 +0300

    last build

--- js/index.js
+++ js/index.js
@@ -6,6 +6,4 @@ import getElements from './getelements';
 import jsonFile from '../loaders/jsonFile.json';
 
 document.querySelector('.lazy-load-js-css').classList.add('hidden');
-
-//check my json loader
 console.log(jsonFile);
