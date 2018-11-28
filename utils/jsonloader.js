afa5ea4bcc4f3321fd21a42f08cbbf653553b028
mkavaliou <mikalai.kavaliou@burberry.com>
2018-11-28 14:22:23 +0300

    fix json loader

--- utils/jsonloader.js
+++ utils/jsonloader.js
@@ -1,6 +1,12 @@
 module.exports = function(source) {
   let value = typeof source === "string" ? JSON.parse(source) : source;
 
+  Object.keys(value).forEach((attribute, i) => {
+    if (!isNaN(+attribute)) {
+      delete value[attribute];
+    }
+  })
+
   value = JSON.stringify(value)
     .replace(/\u2028/g, '\\u2028')
     .replace(/\u2029/g, '\\u2029');
