295c87cc2a38052ff697c833b78a32db9b74edd1
Nikkov17 <kovalyov.k51@gmail.com>
2018-12-05 19:27:48 +0300

    add proxy

--- app/modules/getdata.js
+++ app/modules/getdata.js
@@ -3,12 +3,25 @@ import fetchFactory from './fetchfactory';
 
 export default async function SendFetch(value) {
     let result;
-    let url = `https://newsapi.org/v2/top-headlines?` +
+    const url = `https://newsapi.org/v2/top-headlines?` +
         `sources=${value}&` +
         `apiKey=${constants.APIKEY}`;
     let options = fetchFactory(constants.METHODS.GET);
+    const methodHandler = {
+        get: function(target, property) {
+            let shouldLog = ((property === 'method') ||
+            (property === 'body') ||
+            (property === 'headers'));
 
-    return fetch(url, options)
+            if (shouldLog) {
+                console.log(`${property}: ${target[property]}`);
+            }
+            return (target.property);
+        }
+    };
+    const proxy = new Proxy(options, methodHandler);
+
+    return fetch(url, proxy)
         .then((response) => {
             result = response.json();
             return result;
